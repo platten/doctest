30953d3ee7e3a428260e0211d5581a426707b1de
Roman Volosatovs <roman@profian.com>
2022-04-25 20:51:22 +0200

    feat: add support for optional context
    Signed-off-by: Roman Volosatovs <roman@profian.com>

--- src/main.rs
+++ src/main.rs
@@ -1,61 +1,71 @@
 // SPDX-FileCopyrightText: 2022 Profian Inc. <opensource@profian.com>
 // SPDX-License-Identifier: Apache-2.0
 
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
 use std::fs::File;
 use std::io::{BufRead, BufReader, Read};
+use std::ops::Deref;
 
-use anyhow::Result;
+use anyhow::{anyhow, Result};
 use pulldown_cmark::{CodeBlockKind, Event, Parser, Tag};
 
 trait CodeBlockKindExt {
     /// Determines whether this code block should be included in output.
     ///
     /// This is based on matching KEY=VALUE filters from `/etc/os-release`.
-    fn include(&self, os: &HashMap<String, String>) -> bool;
+    fn include(&self, cx: &HashSet<String>, os: &HashMap<String, String>) -> bool;
 }
 
 impl CodeBlockKindExt for CodeBlockKind<'_> {
-    fn include(&self, os: &HashMap<String, String>) -> bool {
-        match self {
-            Self::Fenced(kind) => {
-                let mut split = kind.as_ref().split(':');
-
-                match split.next() {
-                    Some("sh") => {
-                        for filter in split {
-                            match filter.split_once('=') {
-                                Some((k, v)) if os.get(k).map(|x| &**x) == Some(v) => continue,
-                                _ => return false,
-                            }
-                        }
-
-                        true
-                    }
-
-                    _ => false,
-                }
-            }
-
-            _ => false,
+    fn include(&self, cx: &HashSet<String>, os: &HashMap<String, String>) -> bool {
+        let param = match self {
+            Self::Fenced(k) => match k.split_once(':') {
+                Some(("sh", param)) => param,
+                _ => return k.deref() == "sh", // Include ```sh blocks
+            },
+            _ => return false,
+        };
+        let (c, o) = param.split_once(';').unwrap_or(("", param));
+        if !c.is_empty()
+            && c.split(',')
+                .map(Into::into)
+                .collect::<HashSet<_>>()
+                .intersection(cx)
+                .next()
+                .is_none()
+        {
+            return false;
         }
+        o.is_empty()
+            || o.split_whitespace()
+                .map(|x| x.split_once("=").unwrap())
+                .any(|(k, v)| os.get(k).map(String::as_str) == Some(v))
     }
 }
 
 /// Returns an iterator over the command lines in code blocks based on OS filters.
-fn filter_markdown(os: impl Read, md: &str) -> Result<impl '_ + Iterator<Item = String>> {
+fn filter_markdown<'a>(
+    cx: &'a HashSet<String>,
+    os: impl Read,
+    md: &'a str,
+) -> Result<impl 'a + Iterator<Item = String>> {
     // Read the distribution variables.
-    let mut distro = HashMap::new();
-    for line in BufReader::new(os).lines() {
-        if let Some((lhs, rhs)) = line?.split_once('=') {
-            distro.insert(lhs.to_owned(), rhs.to_owned());
-        }
-    }
+    let os_release = BufReader::new(os)
+        .lines()
+        .map(|r| match r {
+            Ok(line) => line
+                .split_once('=')
+                .map(|(k, v)| (k.into(), v.into()))
+                .ok_or(anyhow!("invalid os-release line: {}", line)),
+            Err(e) => Err(anyhow!(e)),
+        })
+        .collect::<Result<HashMap<_, _>>>()
+        .map_err(|e| anyhow!("failed to read os-release: {}", e))?;
 
-    // Filter the command blocks using the os filters.
+    // Filter the command blocks using the filters.
     let mut dump = false;
     Ok(Parser::new(md).filter_map(move |event| match event {
-        Event::Start(Tag::CodeBlock(block)) if block.include(&distro) => {
+        Event::Start(Tag::CodeBlock(block)) if block.include(cx, &os_release) => {
             dump = true;
             None
         }
@@ -76,17 +86,20 @@ fn main() -> Result<()> {
 
     let cmd = args.next().unwrap();
 
-    let md = match args.next() {
-        Some(md) => std::fs::read_to_string(md)?,
-        None => {
-            eprintln!("Usage: {} <markdown> <os-release>", cmd);
+    let (md, os) = match (args.next(), args.next()) {
+        (Some(md), Some(os)) => (std::fs::read_to_string(md)?, os),
+        _ => {
+            eprintln!("Usage: {} <markdown> <os-release> [<context>]", cmd);
             std::process::exit(1);
         }
     };
 
-    let os = args.next().unwrap_or_else(|| "/etc/os-release".to_owned());
+    let cx = args
+        .next()
+        .map(|s| s.split(',').map(Into::into).collect())
+        .unwrap_or_default();
 
-    for cmd in filter_markdown(File::open(os)?, &md)? {
+    for cmd in filter_markdown(&cx, File::open(os)?, &md)? {
         print!("{}", cmd);
     }
 
@@ -95,10 +108,9 @@ fn main() -> Result<()> {
 
 #[cfg(test)]
 mod test {
-    use crate::filter_markdown;
+    use super::*;
 
-    const OS_RELEASE: &str = r#"
-PRETTY_NAME="Debian GNU/Linux 11 (bullseye)"
+    const OS_RELEASE: &str = r#"PRETTY_NAME="Debian GNU/Linux 11 (bullseye)"
 NAME="Debian GNU/Linux"
 VERSION_ID="11"
 VERSION="11 (bullseye)"
@@ -109,8 +121,7 @@ SUPPORT_URL="https://www.debian.org/support"
 BUG_REPORT_URL="https://bugs.debian.org/"
 "#;
 
-    const MARKDOWN: &str = r#"
-# Welcome!
+    const MARKDOWN: &str = r#"# Welcome!
 
 Welcome to Enarx.
 
@@ -123,10 +134,28 @@ Welcome to Enarx.
 echo fedora
 ```
 
-### Debian
+### Debian or Debian-like (e.g. Ubuntu)
 
-```sh:ID=debian
+```sh:ID=debian ID_LIKE=debian
 echo debian
+```
+
+## Git or SEV
+
+```sh:git,sev;
+echo git or sev
+```
+
+## Not Git
+
+```sh:notgit; ID=debian
+echo notgit
+```
+
+## Git on Debian or Fedora
+
+```sh:git; ID=debian ID=fedora
+echo git on debian or fedora
 ```
 
 ## Build Enarx
@@ -141,10 +170,22 @@ echo enarx
         let mut os = OS_RELEASE.as_bytes();
 
         assert_eq!(
-            filter_markdown(&mut os, MARKDOWN)
-                .unwrap()
-                .collect::<String>(),
-            "echo debian\necho enarx\n"
+            filter_markdown(
+                &{
+                    let mut cx = HashSet::new();
+                    cx.insert("git".into());
+                    cx
+                },
+                &mut os,
+                MARKDOWN
+            )
+            .unwrap()
+            .collect::<String>(),
+            r#"echo debian
+echo git or sev
+echo git on debian or fedora
+echo enarx
+"#
         );
     }
 }
